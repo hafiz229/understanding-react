ede33418662f97e4ef191e24df2273557382af3a
Hafiz <hasnathafiz229>
2021-09-26 02:41:14 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Books from "./components/Books/Books";
+import Device from "./components/Device/Device";
 
 function App() {
+  const [steps, setSteps] = useState(0);
+  const books = [
+    { name: "Small Giants", author: "bo" },
+    { name: "Build to Sell", author: "john" },
+    { name: "Dev Guide", author: "steve" },
+    { name: "Animal Planet", author: "ryans" },
+  ];
+
+  const handleIncreaseSteps = () => {
+    const newStepsCount = steps + 1;
+    setSteps(newStepsCount);
+  };
+  useEffect(() => {
+    console.log("hello");
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>My Steps: {steps}</h3>
+      <button onClick={handleIncreaseSteps}>Walk</button>
+      <Device name="phone" steps={steps} price="13000"></Device>
+      <Books books={books} />
     </div>
   );
 }
